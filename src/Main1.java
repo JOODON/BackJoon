4902ae155636d1c3cff950910a3f375c0650792b
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-10 20:14:15 +0900

    백준 18108번

---
+++ src/Main1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[]args){
+        Scanner in =new Scanner(System.in);
+        int a=in.nextInt();
+        System.out.println(a-543);
+        //백준 18108번
+    }
+}
