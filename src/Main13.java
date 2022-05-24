c9f1b281bd84a40d473b78c0d306d98c8af0a0eb
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-24 22:04:54 +0900

    백준 19050문제

--- src/Main13.java
+++ src/Main13.java
@@ -4,25 +4,13 @@ public class Main13 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
 
-        int a= in.nextInt();
-        String b= in.next();
-        int c=in.nextInt();
-        int result =0;
-        in.close();
+        int a=in.nextInt();
 
-        while (true){
-            if (b.equals("+")){
-                result=a+c;
-            } else if (b.equals("-")) {
-                result=a-c;
-            } else if (b.equals("*")) {
-                result=a*c;
-            }else if (b.equals("=")){
-                break;
-            }
-            else {
-                result=a/c;
-            }
+        for (int i=0;i<=a-1;i++){
+            int b=in.nextInt();
+            int c=in.nextInt();
+            System.out.println(b+c);
         }
     }
 }
+//백준 19050문제
