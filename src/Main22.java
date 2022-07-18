cac6bb353897cf9406f0cd770cce1b18c0000c89
주동호 <100278301+JOODON@users.noreply.github.com>
2022-07-18 10:33:25 +0900

    백준2577번

---
+++ src/Main22.java
@@ -0,0 +1,7 @@
+public class Main22 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
---
+++ src/resion.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class resion {
+    public static void main(String[]args){
+        int [] a={1,2,3,4,5};
+        String b=String.valueOf(a);
+        System.out.println(Arrays.asList(b).indexOf("2"));
+        System.out.println(Arrays.toString(a));
+
+    }
+}
